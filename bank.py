9b5b9fdcaf0df9c70b0fc6668444a24426449dec
dkaraj <36538797+dkaraj@users.noreply.github.com>
2021-11-16 10:33:43 +0100

    Created bank

---
+++ bank.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class BankAccount:
+
+    def __init__(
+        self,
+        name,
+        start_balance,
+        currency,
+        ):
+        return None
+
+    def __int__(self):
+        return None
+
+    def __str__(self):
+        return None
+
+    def __make_history(self, event):
+        return None
+
+    def balance(self):
+        return None
+
+    def holder(self):
+        return None
+
+    def currency(self):
+        return None
+
+    def deposit(self, amount):
+        return None
+
+    def withdraw(self, amount):
+
+        return None
+
+    def transfer_to(self, account, amount):
+        return None
+
+    def history(self):
+        return None
